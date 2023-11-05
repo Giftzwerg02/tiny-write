a76f2a5a5e0505c5ddf0c0966a7e89c9fc689c1e
Giftzwerg02 <chessplayer1@gmx.at>
2023-11-06 00:34:52 +0100

    Build system not broken

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
-fn main() {
-    println!("Hello, world!");
+use gtk::{glib, Application, PropagationPhase, Button, GestureDrag};
+use gtk::{
+    prelude::*, ApplicationWindow, EventController, EventControllerLegacy, Gesture, GestureStylus,
+};
+
+const APP_ID: &str = "org.gtk_rs.HelloWorld1";
+
+fn main() -> glib::ExitCode {
+    let app = Application::builder().application_id(APP_ID).build();
+
+    app.connect_activate(build_ui);
+
+    app.run()
+}
+
+fn build_ui(app: &Application) {
+    let tablet = GestureStylus::builder()
+        .stylus_only(false)
+        .build();
+
+
+
+
+    tablet.connect_ev.
+
+    tablet.connect_up(|t, x, y| {
+        dbg!(t);
+        dbg!(t.device_tool());
+        dbg!(t.backlog());
+    });
+
+
+    tablet.connect_down(|t, x, y| {
+        println!("{} {}", x, y);
+    });
+
+
+    let window = ApplicationWindow::builder()
+        .application(app)
+        .title("amogus")
+        .build();
+
+    window.add_controller(tablet);
+
+    window.present();
 }
